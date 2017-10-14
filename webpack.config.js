996f32cf93d4ee1d48bcd027c798266bc6e5a49f
Benjamin Knox <bknox.contact@gmail.com>
2017-10-14 17:14:22 -0400

    Add SASS compiler

--- webpack.config.js
+++ webpack.config.js
@@ -4,25 +4,31 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 var rootAssetPath = './assets';
 
+var extractSASS = new ExtractTextPlugin('[name].[chunkhash].css');
+
 module.exports = {
   entry: {
     app_js: [
       rootAssetPath + '/js/app.js'
+    ],
+    app_css: [
+      rootAssetPath + '/scss/app.sass'
     ]
   },
   output: {
     path: path.resolve(__dirname, 'webkit-build'),
     publicPath: "http://localhost:8080/",
     filename: '[name].[chunkhash].js',
-    chunkFilename: '[id].[chunkhash].js'
+    chunkFilename: '[id].[chunkhash].chunk'
   },
   resolve: {
-    extensions: ['.js', '.css']
+    extensions: ['.js', '.sass']
   },
   module: {
     loaders: [
-      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+      { test: /\.js$/i, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.jsx$/i, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.sass$/i, loader: extractSASS.extract(['css-loader', 'sass-loader']) }
     ]
   },
   devServer: {
@@ -34,10 +40,10 @@ module.exports = {
     poll: 500
   },
   plugins: [
-    new ExtractTextPlugin('[name].[chunkhash].css'),
+    extractSASS,
     new ManifestRevisionPlugin(path.join('webkit-build', 'manifest.json'), {
       rootAssetPath: rootAssetPath,
-      ignorePaths: ['/js', '/css','/scss']
+      ignorePaths: ['/js', '/sass']
     })
   ]
 };
