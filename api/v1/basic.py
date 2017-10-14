706ddd28054dfa44b8c1f8b9dbfa2c9b021795d2
Benjamin Knox <bknox.contact@gmail.com>
2017-10-14 11:36:31 -0400

    Add API call by React to Flask API

--- api/v1/basic.py
+++ api/v1/basic.py
@@ -1,7 +1,7 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, jsonify
 
 basic_route = Blueprint('basic_route', __name__)
 
 @basic_route.route('/basic')
 def basic():
-  return "{page:'Basic'}"
+    return jsonify({'header':'Hello World!!!!!!!', 'body': 'Called by react from the flask API.'})
--- assets/js/components/app.jsx
+++ assets/js/components/app.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { page: [] };
+  }
+
+  componentDidMount() {
+    fetch(`http://localhost:5000/api/v1/basic`)
+      .then(result => {
+        return result.json();
+      })
+      .then(response => {
+        this.setState({page: response});
+      });
+  }
+
   render() {
     return (
       <div style={{textAlign: 'center'}}>
-        <h1>Hello World!</h1>
+        <h1>{ this.state.page.header }</h1>
+        <p>{ this.state.page.body }</p>
       </div>
     );
   }
