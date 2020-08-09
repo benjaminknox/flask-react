d0e09f5fbebb8a1359ddcb66ec7d6671db7abe5d
Benjamin Knox <bknox.contact@gmail.com>
2020-08-09 00:57:06 +0000

    Fixes export typo

--- js/components/intro.jsx
+++ js/components/intro.jsx
@@ -25,7 +25,7 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-export default function Quill() {
+export default function Intro() {
   const classes = useStyles();
   const [page, setPage] = useState([]);
   const [gif, setGif] = useState("loading");
